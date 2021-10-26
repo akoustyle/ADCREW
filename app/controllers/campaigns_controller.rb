998ec6b37817f20128e7cd158b9385b59c6cddab
O neal Maoukola <maoukola.oneal@gmail.com>
2021-10-26 10:13:21 +0200

    create new campaigns, action and view

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -3,4 +3,19 @@ class CampaignsController < ApplicationController
   def index
   end
 
+  def new
+    @campaign = Campaign.new
+  end
+
+  def create
+    @campaign = Campaign.new(campaign_params)
+  end
+
+  private
+
+  def campaign_params
+    params.require(:campaign).permit(:title, :description)
+  end
+
+
 end
